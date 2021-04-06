2d7f843d8f8b220481e936b1efaa5af13591f367
Farnaz <ffarzipour92@gmail.com>
2021-04-06 10:15:16 +0430

    css

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
-// module.exports = {
-//   chainWebpack: config => {
-// 	if(process.env.NODE_ENV === 'production') {
-// 		config.plugin('optimize-css').tap(([options]) => {
-// 		  options.cssnanoOptions.preset[1].svgo = false
-// 		  return [options]
-// 		})
-// 	}
-//   },
-// }
+module.exports = {
+  chainWebpack: config => {
+	if(process.env.NODE_ENV === 'production') {
+		config.plugin('optimize-css').tap(([options]) => {
+		  options.cssnanoOptions.preset[1].svgo = false
+		  return [options]
+		})
+	}
+  },
+}
